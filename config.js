79e414f000d411e38fb2abc36d40bcaf174080e4
gkishore1805-cpu <gkishore1805@gmail.com>
2026-09-05 12:04:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Copy this file to "config.js" and paste in your real OpenWeatherMap API key.
 // config.js is listed in .gitignore, so your key is never pushed to GitHub.
 
-window.WEATHER_API_KEY = adc3aa9ddd1b57e3cf6c24e085fd8407;
+window.WEATHER_API_KEY = API_KEY;
